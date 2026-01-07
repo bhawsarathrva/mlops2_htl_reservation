c73a5c6626fba27dc8f4983b249282c6653a968b
Athrva Bhawsar <athrvabh124@gmail.com>
2026-01-07 21:35:59 +0530

    commit

--- PROJECT CODE/pipeline/training_pipeline.py
+++ PROJECT CODE/pipeline/training_pipeline.py
@@ -6,17 +6,14 @@ from config.paths_config import *
 
 
 if __name__=="__main__":
-    ### 1. Data Ingestion
-
+    #Data Ingestion
     data_ingestion = DataIngestion(read_yaml(CONFIG_PATH))
     data_ingestion.run()
 
-    ### 2. Data Processing
-
+    #Data Processing
     processor = DataProcessor(TRAIN_FILE_PATH,TEST_FILE_PATH,PROCESSED_DIR,CONFIG_PATH)
     processor.process()
 
-    ### 3. Model Training
-
+    #Model Training
     trainer = ModelTraining(PROCESSED_TRAIN_DATA_PATH,PROCESSED_TEST_DATA_PATH,MODEL_OUTPUT_PATH)
     trainer.run()
